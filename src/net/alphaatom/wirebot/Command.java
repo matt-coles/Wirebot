6f988f6f1d64bf42a96cc02053159e601a99aca9
Matt <mattc65@gmail.com>
2015-07-13 22:05:13 +0100

    Use sender-name over display-name from USERSTATE because we don't know if it will be empty.

--- src/net/alphaatom/wirebot/Command.java
+++ src/net/alphaatom/wirebot/Command.java
@@ -13,13 +13,14 @@ public abstract class Command {
 	/**
 	 * Finds out if the user referenced in {@code cmdinfo} has elevation above that of a
 	 * normal user, if the user-type is not empty or if we know the user is the
-	 * broadcaster - the display-name is the same as the channel name.
+	 * broadcaster - the display-name is the same as the channel name. Use sender-name over
+	 * display-name from USERSTATE because it can be empty.
 	 * 
 	 * @param cmdinfo cmdinfo associated with the command
 	 * @return True if the user has elevation, false if not.
 	 */
 	public boolean userHasElevation(Map<String, String> cmdinfo) {
-		 return (!cmdinfo.get("user-type").equals("") || cmdinfo.get("display-name").equalsIgnoreCase(cmdinfo.get("channel").substring(1)));
+		 return (!cmdinfo.get("user-type").equals("") || cmdinfo.get("sender-name").equalsIgnoreCase(cmdinfo.get("channel").substring(1)));
 	}
 
 }
